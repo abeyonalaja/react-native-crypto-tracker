e79fa49ac7e3d91e74f8148852ba5233ffe94b72
Abey Onalaja <abey.onalaja@gmail.com>
2019-01-13 00:54:22 +0000

    Add styles and deps for redux

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Header} from "./src/components"
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View >
+          <Header/>
       </View>
     );
   }
